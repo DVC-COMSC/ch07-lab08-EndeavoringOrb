10b91b976a1b7414dc7d36c0c36d3c9f233fd5dd
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 15:23:08 -0700

    1st commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-# ******************************
-# Make your Code
-# ******************************
+values = list(map(int, input().split()))
+values.insert(0,int(input()))
+for i in range(len(values)-1):
+    if values[i] > values[i+1]:
+        values[i],values[i+1] = values[i+1],values[i]
+print(values)
